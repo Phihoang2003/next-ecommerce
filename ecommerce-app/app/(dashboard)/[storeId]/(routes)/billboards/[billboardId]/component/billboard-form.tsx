6c1d6b2e07531ed2a9dadf0806992d7c291efb9b
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-13 15:23:00 +0700

    completed onSubmit and onDelete

--- ecommerce-app/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/component/billboard-form.tsx
+++ ecommerce-app/app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/component/billboard-form.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { AlertModal } from "@/components/Modal/alert-modal";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { Button } from "@/components/ui/button";
@@ -11,14 +11,17 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Heading } from "@/components/ui/heading";
+import { ImageUpload } from "@/components/ui/image-upload";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Billboard } from "@prisma/client";
+import axios from "axios";
 import { Trash } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import * as z from "zod";
 
 const formSchema = z.object({
@@ -50,8 +53,40 @@ export const BillboardForm: React.FC<BillBoardFormProps> = ({
       imageUrl: "",
     },
   });
-  const onSubmit = () => {};
-  const onDelete = () => {};
+  const onSubmit = async(data:BillboardFromValue) => {
+    try {
+      setLoading(true);
+      if(initialData){
+        await axios.patch(`/api/${params.storeId}/billboards/${params.billboardId}`,data)
+      }
+      else{
+        await axios.post(`/api/${params.storeId}/billboards`,data)
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/billboards`)
+      toast.success(toastMessage)
+    } catch (error) {
+      toast.error("Some thing went wrong");
+    }
+    finally{
+      setLoading(false);
+    }
+  };
+  const onDelete = async() => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/billboards/${params.billboardId}`);
+      router.refresh();
+      router.push(`/${params.storeId}/billboards`)
+      toast.success("Billboards deleted");
+    } catch (error) {
+      toast.error("Make sure you removed all categories using this billboard first.")
+    }
+    finally{
+      setLoading(false);
+      setOpen(false);
+    }
+  };
   return (
     <>
       <AlertModal
@@ -79,20 +114,21 @@ export const BillboardForm: React.FC<BillBoardFormProps> = ({
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8  w-full"
+          className="space-y-8 w-full"
         >
-          <div className="grid grid-cols-3 gap-8">
+          
             <FormField
               control={form.control}
-              name="label"
+              name="imageUrl"
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Backgroud Image</FormLabel>
                   <FormControl>
-                    <Input
+                    <ImageUpload
                       disabled={loading}
-                      placeholder="Store name"
-                      {...field}
+                      value={field.value ? [field.value] : []}
+                      onChange={(url) => field.onChange(url)}
+                      onRemove={() => field.onChange("")}
                     />
                   </FormControl>
 
@@ -101,30 +137,31 @@ export const BillboardForm: React.FC<BillBoardFormProps> = ({
               )}
             />
 
-            <div className="md:grid md:grid-cols-3 gap-8">
-
-            <FormField
-              control={form.control}
-              name="label"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Label</FormLabel>
-                  <FormControl>
-                    <Input disabled={loading} placeholder="Billboard label" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            <div className="md:grid md:grid-cols-3 gap-8 ">
+              <FormField
+                control={form.control}
+                name="label"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Label</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="Billboard label"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
             </div>
-          </div>
+          
           <Button disabled={loading} className="ml-auto" type="submit">
             {action}
           </Button>
         </form>
       </Form>
-
-      
     </>
   );
 };
