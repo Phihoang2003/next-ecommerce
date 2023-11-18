611a67f1fb3d7ed09633f84b28443a903ace90c3
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-18 18:59:53 +0700

    create data-table

--- ecommerce-app/app/(dashboard)/[storeId]/(routes)/billboards/component/client.tsx
+++ ecommerce-app/app/(dashboard)/[storeId]/(routes)/billboards/component/client.tsx
@@ -1,20 +1,25 @@
 "use client"
 import { Button } from '@/components/ui/button'
 import { Heading } from '@/components/ui/heading'
+import { Billboard } from '@prisma/client'
 import { Separator } from '@radix-ui/react-separator'
 import { Plus } from 'lucide-react'
 import { useParams, useRouter } from 'next/navigation'
 
 import React from 'react'
+import { BillboardColumn } from './columns'
 
-export const BillboardClient = () => {
+interface BillboardClientProps{
+  data:BillboardColumn[]
+}
+export const BillboardClient:React.FC<BillboardClientProps> = ({data}) => {
   const router=useRouter();
   const params=useParams();
   return (
     <>
         <div className='flex items-center justify-between'>
             <Heading
-                title='Billboards (0)'
+                title={`Billboards (${data?.length})`}
                 description='Manage billboards for your store'
             />
 
--- ecommerce-app/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ ecommerce-app/app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import { BillboardClient } from './component/client'
+import { db } from '@/lib/db'
+import { BillboardColumn } from './component/columns'
+import {format} from "date-fns"
+const BillboardPage = async({
+  params
+}:{params:{storeId:string}}) => {
+  const billboards=await db.billboard.findMany({
+    where:{
+      storeId:params.storeId
+    },
+    orderBy:{
+      createdAt:"desc"
+    }
+  })
 
-const BillboardPage = () => {
+  const formattedBillboards:BillboardColumn[]=billboards.map((item)=>(
+    {
+      id:item.id,
+      label:item.label,
+      createdAt:format(item.createdAt,"MMMM mo,yyyy")
+    }
+  ))
+  
+
   return (
     <div className='flex-1 p-8 pt-6 space-y-4'>
-        <BillboardClient/>
+        <BillboardClient data={formattedBillboards}/>
     </div>
   )
 }
