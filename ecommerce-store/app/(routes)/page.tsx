d086e907158cd46e4d4ede30d555d381485bde1a
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-25 17:56:23 +0700

    complete billboard for client

--- ecommerce-store/app/(routes)/page.tsx
+++ ecommerce-store/app/(routes)/page.tsx
@@ -1,8 +1,17 @@
+import getBillBoard from "@/actions/get-billboard"
+import Billboard from "@/components/billboard"
+import Container from "@/components/ui/container"
 
 
-const HomePage = () => {
+
+const HomePage =async () => {
+  const billboard=await getBillBoard("822e3083-b78a-4371-b5b6-d40c9d2ed564")
   return (
-    <div className='text-pink-600'>Hello store</div>
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
   )
 }
 
