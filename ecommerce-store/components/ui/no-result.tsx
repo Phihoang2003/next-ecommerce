d2e5086d8c6fddafb86826e9929643f20858506e
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-25 20:52:28 +0700

    create component no-results

---
+++ ecommerce-store/components/ui/no-result.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NoResult = () => {
+  return (
+    <div className='flex items-center justify-center w-full h-full text-neutral-500'>
+        No results found.
+
+    </div>
+  )
+}
+
+export default NoResult
