ed6c72500ca07bc40144d4dc8326b24f3e824a1c
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-25 20:51:46 +0700

    add currency format

---
+++ ecommerce-store/components/ui/currency.tsx
@@ -0,0 +1,26 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+interface CurrencyProps{
+    value?:string|number,
+}
+const Currency:React.FC<CurrencyProps> = ({value}) => {
+    const [isMouted,setIsMouted]=useState(false);
+    useEffect(()=>{
+        setIsMouted(true)
+    },[])
+    if(!isMouted){
+        return null;
+    }
+  return (
+    <div className='font-semibold'>
+        {formatter.format(Number(value))}
+    </div>
+  )
+}
+
+export default Currency
