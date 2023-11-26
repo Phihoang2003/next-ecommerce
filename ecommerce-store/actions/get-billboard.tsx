db9952ce6c0d49acb3cdb7a7b1205dd3b50139c1
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-26 08:26:08 +0700

    update get biilboard

--- ecommerce-store/actions/get-billboard.tsx
+++ ecommerce-store/actions/get-billboard.tsx
@@ -3,7 +3,7 @@ import { BillBoard } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_URL}/billboards`;
 const getBillBoard = async (id:string): Promise<BillBoard> => {
   const res = await fetch(`${URL}/${id}`);
-  console.log("hello");
+  
   
   return res.json();
 };
