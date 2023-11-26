6c8b8f938f161cd6a164445af5e1a46e44a8eb70
Dang Hoang Phi <hoangphi1232003@gmail.com>
2023-11-26 20:24:32 +0700

    update navbar actions

--- ecommerce-store/components/navbar-actions.tsx
+++ ecommerce-store/components/navbar-actions.tsx
@@ -3,20 +3,28 @@
 import { ShoppingBag } from "lucide-react"
 import Button from "./ui/button"
 import {useState,useEffect} from "react"
+import useCart from "@/hooks/use-cart"
+import { useRouter } from "next/navigation"
 const NavbarActions = () => {
     const [isMouted,setIsMouted]=useState(false)
+    const router=useRouter();
     useEffect(()=>{
         setIsMouted(true);
     },[])
+    const cart =useCart()
     if(!isMouted){
         return null;
     }
+    const onClick=()=>{
+        router.push("/cart")
+    }
+    
   return (
     <div className="ml-auto flex items-center gap-x-4">
-        <Button className="flex items-center rounded-full bg-black px-4 py-2">
+        <Button onClick={onClick} className="flex items-center rounded-full bg-black px-4 py-2">
             <ShoppingBag size={20} color="white"/>
             <span className="ml-2 text-sm text-white font-medium">
-            0
+            {cart.items.length}
         </span>
         </Button>
         
